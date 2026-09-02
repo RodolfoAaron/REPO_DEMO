61d1aaad032fc41174f237bf3cea140b96b1eaa4
RodolfoAaron <rodolfo.machco@upch.pe>
2026-09-02 18:04:21 -0500

    Add files via upload

---
+++ AARON_CV/PERFILCV.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Cambiar enlace activo en la barra superior al hacer scroll
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.nav-links a');
+
+    window.addEventListener('scroll', () => {
+        let current = '';
+
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            if (pageYOffset >= (sectionTop - 200)) {
+                current = section.getAttribute('id');
+            }
+        });
+
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+            if (link.getAttribute('href') === `#${current}`) {
+                link.classList.add('active');
+            }
+        });
+    });
+
+});
